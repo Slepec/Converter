541570a423cb8237c8d15f00b7732b4b26b73f70
Dmytro Sihov <dima.sigov1@gmail.com>
2022-05-04 19:18:05 +0300

    Добавьте файлы проекта.

---
+++ Converter/Form1.Designer.cs
@@ -0,0 +1,442 @@
+﻿
+namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelSm = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panelInch = new System.Windows.Forms.Panel();
+            this.panelCel = new System.Windows.Forms.Panel();
+            this.panelFar = new System.Windows.Forms.Panel();
+            this.panelPound = new System.Windows.Forms.Panel();
+            this.panelKil = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            this.panelSm.SuspendLayout();
+            this.panelInch.SuspendLayout();
+            this.panelCel.SuspendLayout();
+            this.panelFar.SuspendLayout();
+            this.panelPound.SuspendLayout();
+            this.panelKil.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(18, 64);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown2.Location = new System.Drawing.Point(18, 57);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Location = new System.Drawing.Point(18, 66);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Enabled = false;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown4.Location = new System.Drawing.Point(19, 66);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown4.TabIndex = 5;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.DecimalPlaces = 2;
+            this.numericUpDown5.Enabled = false;
+            this.numericUpDown5.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown5.Location = new System.Drawing.Point(19, 57);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown5.TabIndex = 4;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.DecimalPlaces = 2;
+            this.numericUpDown6.Enabled = false;
+            this.numericUpDown6.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown6.Location = new System.Drawing.Point(19, 64);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown6.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(100, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Конвертувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(100, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Конвертувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(100, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Конвертувати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panelSm
+            // 
+            this.panelSm.AutoSize = true;
+            this.panelSm.Controls.Add(this.label1);
+            this.panelSm.Controls.Add(this.numericUpDown1);
+            this.panelSm.Location = new System.Drawing.Point(319, 92);
+            this.panelSm.Name = "panelSm";
+            this.panelSm.Size = new System.Drawing.Size(200, 100);
+            this.panelSm.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сантиветри";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Дюйми";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Цельсій";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Фаренгейт";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Фунти";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Кілограми";
+            // 
+            // panelInch
+            // 
+            this.panelInch.AutoSize = true;
+            this.panelInch.Controls.Add(this.label2);
+            this.panelInch.Controls.Add(this.numericUpDown6);
+            this.panelInch.Location = new System.Drawing.Point(638, 92);
+            this.panelInch.Name = "panelInch";
+            this.panelInch.Size = new System.Drawing.Size(200, 100);
+            this.panelInch.TabIndex = 15;
+            // 
+            // panelCel
+            // 
+            this.panelCel.AutoSize = true;
+            this.panelCel.Controls.Add(this.label3);
+            this.panelCel.Controls.Add(this.numericUpDown2);
+            this.panelCel.Location = new System.Drawing.Point(319, 226);
+            this.panelCel.Name = "panelCel";
+            this.panelCel.Size = new System.Drawing.Size(200, 100);
+            this.panelCel.TabIndex = 16;
+            // 
+            // panelFar
+            // 
+            this.panelFar.AutoSize = true;
+            this.panelFar.Controls.Add(this.label4);
+            this.panelFar.Controls.Add(this.numericUpDown5);
+            this.panelFar.Location = new System.Drawing.Point(638, 226);
+            this.panelFar.Name = "panelFar";
+            this.panelFar.Size = new System.Drawing.Size(200, 100);
+            this.panelFar.TabIndex = 17;
+            // 
+            // panelPound
+            // 
+            this.panelPound.AutoSize = true;
+            this.panelPound.Controls.Add(this.label5);
+            this.panelPound.Controls.Add(this.numericUpDown3);
+            this.panelPound.Location = new System.Drawing.Point(319, 351);
+            this.panelPound.Name = "panelPound";
+            this.panelPound.Size = new System.Drawing.Size(200, 100);
+            this.panelPound.TabIndex = 17;
+            // 
+            // panelKil
+            // 
+            this.panelKil.AutoSize = true;
+            this.panelKil.Controls.Add(this.label6);
+            this.panelKil.Controls.Add(this.numericUpDown4);
+            this.panelKil.Location = new System.Drawing.Point(638, 351);
+            this.panelKil.Name = "panelKil";
+            this.panelKil.Size = new System.Drawing.Size(200, 100);
+            this.panelKil.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(537, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Змінити";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(537, 266);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 27);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Змінити";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Location = new System.Drawing.Point(537, 394);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 27);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Змінити";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 605);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panelFar);
+            this.Controls.Add(this.panelPound);
+            this.Controls.Add(this.panelKil);
+            this.Controls.Add(this.panelCel);
+            this.Controls.Add(this.panelInch);
+            this.Controls.Add(this.panelSm);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            this.panelSm.ResumeLayout(false);
+            this.panelSm.PerformLayout();
+            this.panelInch.ResumeLayout(false);
+            this.panelInch.PerformLayout();
+            this.panelCel.ResumeLayout(false);
+            this.panelCel.PerformLayout();
+            this.panelFar.ResumeLayout(false);
+            this.panelFar.PerformLayout();
+            this.panelPound.ResumeLayout(false);
+            this.panelPound.PerformLayout();
+            this.panelKil.ResumeLayout(false);
+            this.panelKil.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panelSm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panelInch;
+        private System.Windows.Forms.Panel panelCel;
+        private System.Windows.Forms.Panel panelFar;
+        private System.Windows.Forms.Panel panelPound;
+        private System.Windows.Forms.Panel panelKil;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Converter/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Point location = panelSm.Location;
+            panelSm.Location = panelInch.Location;
+            panelInch.Location = location;
+            if (numericUpDown6.Enabled)
+            {
+                numericUpDown6.Enabled = false;
+                numericUpDown1.Enabled = true;
+            }
+            else
+            {
+                numericUpDown6.Enabled = true;
+                numericUpDown1.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Point location = panelCel.Location;
+            panelCel.Location = panelFar.Location;
+            panelFar.Location = location;
+            if (numericUpDown5.Enabled)
+            {
+                numericUpDown5.Enabled = false;
+                numericUpDown2.Enabled = true;
+            }
+            else
+            {
+                numericUpDown5.Enabled = true;
+                numericUpDown2.Enabled = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Point location = panelPound.Location;
+            panelPound.Location = panelKil.Location;
+            panelKil.Location = location;
+            if (numericUpDown4.Enabled)
+            {
+                numericUpDown4.Enabled = false;
+                numericUpDown3.Enabled = true;
+            }
+            else
+            {
+                numericUpDown4.Enabled = true;
+                numericUpDown3.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!numericUpDown1.Enabled)
+            {
+                numericUpDown1.Value = numericUpDown6.Value * 2.54m;
+            }
+            else
+            {
+                numericUpDown6.Value = numericUpDown1.Value / 2.54m;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!numericUpDown2.Enabled)
+            {
+                numericUpDown2.Value = (numericUpDown5.Value - 32) * (5m / 9m);
+            }
+            else
+            {
+                numericUpDown5.Value = (9m / 5m) * numericUpDown2.Value +32;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!numericUpDown3.Enabled)
+            {
+                numericUpDown3.Value = numericUpDown4.Value / 0.457m;
+            }
+            else
+            {
+                numericUpDown4.Value = numericUpDown3.Value * 0.457m; 
+            }
+        }
+    }
+}
